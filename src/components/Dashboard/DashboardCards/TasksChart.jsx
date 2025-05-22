f548141150bceed927b703799c2b8deff9feebfa
brian2012 <brianchoo2012@gmail.com>
2025-05-22 16:59:54 +0800

    feat: added pie chart

--- src/components/Dashboard/DashboardCards/TasksChart.jsx
+++ src/components/Dashboard/DashboardCards/TasksChart.jsx
@@ -1,17 +1,40 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { TasksContext } from "../../../context/tasksContext";
 import Card from "../../Card/Card";
 
-const Title = styled.div`
-  font-size: 20px;
-  color: #537178;
-  margin-bottom: 12px;
+const ChartContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PieChart = styled.div`
+  width: 110px;
+  height: 110px;
+  border-radius: 50%;
+  background: ${(props) => `conic-gradient(
+    #5285EC 0deg ${props.completedDegrees}deg,
+    #E5E5E5 ${props.completedDegrees}deg 360deg
+  )`};
 `;
 
 const TasksChart = () => {
+  const { tasks } = useContext(TasksContext);
+
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
+  const completedPercentage =
+    totalTasks > 0 ? (completedTasks / totalTasks) * 100 : 0;
+
+  const completedDegrees = (completedPercentage / 100) * 360;
+
   return (
     <>
       <Card>
-        <Title>Tasks Chart</Title>
+        <ChartContainer>
+          <PieChart completedDegrees={completedDegrees} />
+        </ChartContainer>
       </Card>
     </>
   );
